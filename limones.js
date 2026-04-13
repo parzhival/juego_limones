f6d4c369524381e9cbcdfd05ceb6e58321ff1678
parzhival <jean08070225@gmail.com>
2026-04-12 20:26:33 -0500

    se agrega puntaje y vidas

--- limones.js
+++ limones.js
@@ -11,6 +11,8 @@ let personajeX=canvas.width/2;
 let personajeY=canvas.height-(ALTURA_SUELO+ALTO_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=0;
+let puntaje=0;
+let vidas=3;
 
 function iniciar(){
     dibujarSuelo();
@@ -53,21 +55,26 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY + 10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
+    detectarPiso();
 }
-function detectarColision(){
+function detectarAtrapado(){
     if(limonX+ANCHO_LIMON > personajeX && 
         limonX < personajeX+ANCHO_PERSONAJE &&
         limonY+ALTO_LIMON > personajeY &&
         limonY < personajeY+ALTO_PERSONAJE){
-        //alert("ATRAPADO!!");
         aparecerLimon();
+        puntaje=puntaje+1;
+        mostrarEnSpan("txtPuntaje",puntaje);
     }
 }
-function probarAleatorio(){
-    let aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
+function detectarPiso(){
+    if(limonY+ALTO_LIMON==canvas.height-ALTURA_SUELO){
+        aparecerLimon();
+        vidas=vidas-1
+        mostrarEnSpan("txtVidas",vidas) }
 }
+
 function aparecerLimon(){
     limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
     limonY=0;
